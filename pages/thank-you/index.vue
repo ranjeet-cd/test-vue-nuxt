0dd9716c67e8c50ca7321dd92c15fc1168e61bde
Ranjeet Hange <ranjeet.hange@canadadrives.ca>
2022-03-18 14:48:44 -0700

    test change added thank you

---
+++ pages/thank-you/index.vue
@@ -0,0 +1,17 @@
+<template>
+<h2> Thank you </h2>
+</template>
+
+<script>
+
+export default {
+  name: 'ThankYou',
+
+mounted () {
+  console.log(" Mounted thank you ...")
+}
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
